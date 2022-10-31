fc7132cf5230fcbbe2ed080f78cf9de554185d89
Steven Bryant <stevelad_83@yahoo.com>
2022-10-31 19:08:30 -0400

    grabbing dom elements, initial state, and console.log for event listener on button one

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-/* Imports */
-
 /* Get DOM Elements */
+const btnOne = document.getElementById('button-one');
+const btnTwo = document.getElementById('button-two');
+const btnThree = document.getElementById('button-three');
+
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('total');
+
+/* add images */
 
 /* State */
-
+let wins = 0;
+let total = 0;
 /* Events */
 
 /* Display Functions */
+
+btnOne.addEventListener('click', () => {
+    console.log('clicking button one!');
+});
 
 // (don't forget to call any display functions you want to run on page load!)
