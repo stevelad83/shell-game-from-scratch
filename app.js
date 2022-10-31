a4410a42090ee89723c9073e887cc39e3327f6e8
Steven Bryant <stevelad_83@yahoo.com>
2022-10-31 19:58:35 -0400

    fixed css layout

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const btnOne = document.getElementById('button-one');
 const btnTwo = document.getElementById('button-two');
 const btnThree = document.getElementById('button-three');
 
+const img1 = document.getElementById('image-one');
+const img2 = document.getElementById('image-two');
+const img3 = document.getElementById('image-three');
+
 const winsEl = document.getElementById('wins');
 const lossesEl = document.getElementById('losses');
 const totalEl = document.getElementById('total');
@@ -14,10 +18,20 @@ let wins = 0;
 let total = 0;
 /* Events */
 
-/* Display Functions */
-
 btnOne.addEventListener('click', () => {
     console.log('clicking button one!');
+    total++;
+    resetPearls();
+    const hidingPLace = Math.ceil(Math.random() * 3);
+    console.log(hidingPLace);
 });
 
+/* Display Functions */
+
 // (don't forget to call any display functions you want to run on page load!)
+
+function resetPearls() {
+    img1.classList.remove('reveal');
+    img2.classList.remove('reveal');
+    img3.classList.remove('reveal');
+}
